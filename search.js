2c7afd077cf9307e3ed64f39a1122cebe6433be5
Aaron <110748676+SurfWeb3@users.noreply.github.com>
2023-02-19 01:30:11 -0800

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,18 @@
+//var searchToken = () => {
+//    var searchIn = document.getElementById("searchInput");
+//    var filter = searchIn.value.toUpperCase();
+//    var parentEle = document.getElementById("token_list");
+//    var tokenEle = parentEle.getElementsByTagName("span");
+//    for (let i = 0; i < tokenEle.length; i++) {
+//        tokenSym = tokenEle[i].textContent || tokenEle[i].innerText;
+//        if(tokenSym.toUpperCase().indexOf(filter) > -1) {
+//            tokenEle[i].parentElement.style.display = "";
+//        } else {
+//            tokenEle[i].parentElement.style.display = "none";
+//        }
+//    }
+//}
+
 var searchToken = () => {
     var searchIn = document.getElementById("searchInput");
     var filter = searchIn.value.toUpperCase();
@@ -5,12 +20,13 @@ var searchToken = () => {
     var tokenEle = parentEle.getElementsByTagName("span");
     for (let i = 0; i < tokenEle.length; i++) {
         tokenSym = tokenEle[i].textContent || tokenEle[i].innerText;
-        if(tokenSym.toUpperCase().indexOf(filter) > -1) {
+        if(tokenSym.toUpperCase().startsWith(filter)) {
             tokenEle[i].parentElement.style.display = "";
         } else {
             tokenEle[i].parentElement.style.display = "none";
         }
     }
 }
+
 
 document.getElementById("searchInput").onkeyup = searchToken;
