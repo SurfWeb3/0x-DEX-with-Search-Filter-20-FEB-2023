13634b9def690ad005c00fb715a1e29189ee6e4b
Aaron <110748676+SurfWeb3@users.noreply.github.com>
2023-02-20 04:28:39 -0800

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,4 @@
+//THIS WAS THE ORIGINAL SEARCH FUNCTION FOR THE DEX
 //var searchToken = () => {
 //    var searchIn = document.getElementById("searchInput");
 //    var filter = searchIn.value.toUpperCase();
@@ -13,6 +14,7 @@
 //    }
 //}
 
+//THIS HAS ALPHABETIC FILTER
 var searchToken = () => {
     var searchIn = document.getElementById("searchInput");
     var filter = searchIn.value.toUpperCase();
@@ -27,6 +29,27 @@ var searchToken = () => {
         }
     }
 }
+
+
+
+
+//THIS HAS UPDATE IN TO-TOKEN-AMOUNT
+var searchTokenAmountUpdate = () => {
+    var searchIn = document.getElementById("searchInput");
+    var filter = searchIn.value.toUpperCase();
+    var parentEle = document.getElementById("token_list");
+    var tokenEle = parentEle.getElementsByTagName("span");
+    for (let i = 0; i < tokenEle.length; i++) {
+        tokenSym = tokenEle[i].textContent || tokenEle[i].innerText;
+        if(tokenSym.toUpperCase().indexOf(filter) > -1) {
+            tokenEle[i].parentElement.style.display = "";
+        } else {
+            tokenEle[i].parentElement.style.display = "none";
+        }
+    }
+}
+
+
 
 
 document.getElementById("searchInput").onkeyup = searchToken;
